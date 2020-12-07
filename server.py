98d293f55d8bc8ea04e16e7b0ab50a99ddf4aa16
shaiyis <56585128+shaiyis@users.noreply.github.com>
2020-12-07 23:23:31 +0200

    server part 2

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
-import socket
+import socket,sys
+
+
+# see os.path
+
+TCP_IP = '0.0.0.0'
+TCP_PORT = int(sys.argv[1])
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-server.bind(('', 12345))
-
+server.bind((TCP_IP, TCP_PORT))
 server.listen(5)
 
 while True:
     client_socket, client_address = server.accept()
-
     data = client_socket.recv(100)
     print('Received: ', data)
-    dataArray = data.decode().split(" ")
-    if dataArray[1] == "/":
+    dataArray = data.decode().split("\r\n")
+    ask = dataArray[0]
+    path = ask.split(" ")[1]
+    if path == "/":
         f = open("files/index.html", "r")
+        lines = f.readlines()
         while f.readline():
             client_socket.send(str(f.readlines()).encode())
+    elif path == "":
+        pass
+        # jpg, ico
+    elif path == "/redirect":
+        # send result.html
+        pass
+    else:
+        # all other files
+        pass
     client_socket.close()
     print('Client disconnected')
